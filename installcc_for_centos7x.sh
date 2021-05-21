0a1b0a7b2c498f8ad3045590e1b2b444fd3f5022
learnin9 <learnin9@gmail.com>
2021-05-21 10:37:54 +0800

    Update installcc_for_centos7x.sh

--- installcc_for_centos7x.sh
+++ installcc_for_centos7x.sh
@@ -466,10 +466,10 @@ function astercc_install() {
 
 	./install.sh -dbu=root -dbpw=$mysql_root_pw -amiu=$amiu -amipw=$amipw -allbydefault
 	
-#	cd /usr/src
-#	rm -rf /usr/src/phoneareas.sql
-#	wget $cdnmirror/phoneareas.sql?v=20170613
-#	mysql -uroot -p$mysql_root_pw astercc10</usr/src/phoneareas.sql
+	cd /usr/src
+	rm -rf /usr/src/phoneareas.sql
+	wget $cdnmirror/phoneareas.sql?v=20170613 -O /usr/src/phoneareas.sql
+	mysql -uroot -p$mysql_root_pw astercc10</usr/src/phoneareas.sql
 	echo -e "\e[32mAsterCC Commercial Install OK!\e[m"
 }
 
